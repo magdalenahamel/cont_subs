b370a62c5178752e6f6157b6d57f29cb0e2db226
magdalenahamel <>
2023-01-25 11:14:39 +1100

    Add main.py file

--- ozstar_run_example.sh
+++ ozstar_run_example.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH -c 4      # cores requested
+#SBATCH --mem=10  # memory in Mb
+#SBATCH -o outfile  # send stdout to outfile
+#SBATCH -e errfile  # send stderr to errfile
+#SBATCH -t 0:01:00  # time requested in hour:minute:second
+
 module load python
 python hello.py
