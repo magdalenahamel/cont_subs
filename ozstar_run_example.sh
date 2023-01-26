dafd29987d82f9f0461b36fabe2322f579356e8b
magdalenahamel <>
2023-01-26 19:01:04 +1100

    Add main.py file

--- ozstar_run_example.sh
+++ ozstar_run_example.sh
@@ -2,9 +2,7 @@
 #SBATCH -N 1      # nodes requested
 #SBATCH -n 1      # tasks requested
 #SBATCH -c 4      # cores requested
-#SBATCH --mem=10  # memory in Mb
-#SBATCH -o outfile  # send stdout to outfile
-#SBATCH -e errfile  # send stderr to errfile
+#SBATCH --mem=1G  # memory in Mb
 #SBATCH -t 0:01:00  # time requested in hour:minute:second
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=magdalena.hamel@gmail.com
