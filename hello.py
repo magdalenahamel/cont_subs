110e737870dd68cc4d657c46c3ae41318f7903b0
magdalenahamel <>
2023-01-26 18:55:05 +1100

    Add main.py file

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 import numpy as np
 import ppxf
 import matplotlib.pyplot as plt
-a = np.linspace(0,10, 1000)
-b = np.random.random(1000)
+a = np.linspace(0,10, 10)
+b = np.random.random(10)
 print('a',a)
 print('b', b)
 plt.plot(a,b)
